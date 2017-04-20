addc282d2bffff19f0c7851366c51d56b1808334
Shaikh Faisal Hossain <faisalsaptarshi@gmail.com>
2017-04-20 14:40:59 +0600

    Funtionality Stablized !!!!

--- app/src/main/java/com/developersbd/nabannapro/FourFragment.java
+++ app/src/main/java/com/developersbd/nabannapro/FourFragment.java
@@ -3,7 +3,6 @@ package com.developersbd.nabannapro;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
 import android.view.View;
@@ -11,23 +10,19 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 
 /**
- * Created by TC on 3/30/2017.
+ * Created by TC on 4/15/2017.
  */
 
-public class ThirdFragement extends Fragment {
-    View myview;
+public class FourFragment extends Fragment {
     ImageButton btn1;
     ImageButton btn2;
     ImageButton btn3;
-    @Nullable
-    @Override
-
-
+    View myview;
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        myview= inflater.inflate(R.layout.thirdlayout,container,false);
-        btn1= (ImageButton) myview.findViewById(R.id.frBtn4);
-        btn2= (ImageButton) myview.findViewById(R.id.frBtn5);
-        btn3= (ImageButton) myview.findViewById(R.id.frBtn6);
+        myview=inflater.inflate(R.layout.fourthlayout,container,false);
+        btn1= (ImageButton) myview.findViewById(R.id.frBtn7);
+        btn2= (ImageButton) myview.findViewById(R.id.frBtn8);
+        btn3= (ImageButton) myview.findViewById(R.id.frBtn9);
         btn1.setOnTouchListener(new View.OnTouchListener() {
             @Override
             public boolean onTouch(View v, MotionEvent event) {
@@ -52,7 +47,10 @@ public class ThirdFragement extends Fragment {
                 switch (event.getAction())
                 {
                     case MotionEvent.ACTION_UP:
-                       btn2.setAlpha(255);
+                        FragmentManager fragmentManager=getFragmentManager();
+                        fragmentManager.beginTransaction().replace(R.id.content_main,
+                                new ThirdFragement()).commit();
+                        btn2.setAlpha(255);
                         break;
                     case MotionEvent.ACTION_DOWN:
                         btn2.setAlpha(100);break;
@@ -67,9 +65,7 @@ public class ThirdFragement extends Fragment {
                 switch (event.getAction())
                 {
                     case MotionEvent.ACTION_UP:
-                        FragmentManager fragmentManager=getFragmentManager();
-                        fragmentManager.beginTransaction().replace(R.id.content_main,
-                                new FourFragment()).commit();
+
                         btn3.setAlpha(255);
                         break;
                     case MotionEvent.ACTION_DOWN:
